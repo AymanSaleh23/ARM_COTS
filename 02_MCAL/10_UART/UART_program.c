b52ed189e97a38ce2e87baaf443640dd0adb6757
Ayman Saleh <ayman.saleh.zien@gmail.com>
2022-09-25 12:40:26 +0200

    UART Universal Asynchronous Receiver Transmitter Driver V.2

--- 02_MCAL/10_UART/UART_program.c
+++ 02_MCAL/10_UART/UART_program.c
@@ -1,11 +1,11 @@
 
 /****************************************************/
 /*	Author	:Ayman Saleh							*/
-/*	Date	:20 SEP 2022 							*/
-/*	Version	:V01		 							*/
+/*	Version :V01	Date	:20 SEP 2022 			*/
+/*	Version	:V02	Date	:25 SEP 2022 			*/
 /****************************************************/
 
-
+/*		Baudrate is fixed as 115200		*/
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
 
@@ -16,9 +16,9 @@
 void MUART_voidInit		(void){
 	/*	For 8MHz Clock System		*/
 	/*	9600	:52.08 = 0x34,0x1 	*/
-	/*	115200	:4.34  = 0x04,0x6 	*/
-	UART1 -> BRR_reg.Fraction = 0x1;
-	UART1 -> BRR_reg.Mantissa = 0x34;
+	/*	115200	:4.34  = 0x04,0x5 	*/
+	UART1 -> BRR_reg.Fraction = 0x4;
+	UART1 -> BRR_reg.Mantissa = 0x5;
 	/*	Enable Tx	*/
 	UART1 -> CR1_reg.TE = 1;
 	/*	Enable Rx	*/
@@ -28,7 +28,7 @@ void MUART_voidInit		(void){
 	/*	Reset the Status Register	*/
 	UART1->SR_reg.Value= 0;
 }
-void MUART_voidTransmit	(u8 *Copy_u8Arr []){
+void MUART_voidTransmit	(char *Copy_u8Arr){
 	u32 LOC_u32I = 0;
 	while (Copy_u8Arr[LOC_u32I] != '\0'){
 		/*	Put value toe be transmitted	*/
@@ -40,7 +40,15 @@ void MUART_voidTransmit	(u8 *Copy_u8Arr []){
 }
 u8	 MUART_u8Receive	(void){
 	u8 LOC_u8Data = 0;
-	while ((UART1 ->SR_reg.SR_bits.RXNE) == 0);
+	u16 LOC_u16TimeOut = 0;
+	while ((UART1 ->SR_reg.SR_bits.RXNE) == 0){
+		LOC_u16TimeOut++;
+
+		/*	Can be implemented by System Tick	*/
+		if (LOC_u16TimeOut >= RECEIVE_TIMEOUT){
+			break;
+		}
+	}
 	LOC_u8Data = UART1 ->DR_reg;
 	return (u8) LOC_u8Data;
 }
